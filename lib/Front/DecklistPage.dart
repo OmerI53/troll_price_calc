ddb3acc3eb926b61d079ba73ac669a14b156be3f
OmerI53 <omer.islam53@hotmail.com>
2023-03-06 18:29:17 +0300

    added remove card option

--- lib/Front/DecklistPage.dart
+++ lib/Front/DecklistPage.dart
@@ -13,6 +13,11 @@ class _DecklistPageState extends State<DecklistPage> {
   late List<double> priceArr = priceCalc(widget.decklist);
   late double sidelenght = widget.decklist[2].length as double;
   late String title = widget.title;
+  bool removeItems = false;
+  List removedMain = [];
+  List removedSide = [];
+  List removedExtra = [];
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -48,7 +53,8 @@ class _DecklistPageState extends State<DecklistPage> {
                     padding: const EdgeInsets.all(4),
                     // ignore: sort_child_properties_last
                     children: List.generate(widget.decklist[0].length, (index) {
-                      return CardView(widget.decklist[0][index]);
+                      return CardView(widget.decklist[0][index], index,
+                          removeItems, removedMain);
                     }),
                     crossAxisCount: 10,
                   ),
@@ -62,7 +68,6 @@ class _DecklistPageState extends State<DecklistPage> {
                   color: Colors.lightBlue.withOpacity(0.4),
                   width: 1100,
                   height: 154,
-                  /*
                   child: GridView.count(
                     childAspectRatio: 0.3,
                     crossAxisSpacing: 7,
@@ -70,11 +75,12 @@ class _DecklistPageState extends State<DecklistPage> {
                     padding: const EdgeInsets.all(4),
                     // ignore: sort_child_properties_last
                     children: List.generate(widget.decklist[2].length, (index) {
-                      return CardView(widget.decklist[2][index]);
+                      return CardView(widget.decklist[2][index], index,
+                          removeItems, removedSide);
                     }),
-                    crossAxisCount: widget.decklist[2].length,
+                    // ignore: unnecessary_null_comparison
+                    crossAxisCount: 15,
                   ),
-                  */
                 ),
                 const SizedBox(
                   height: 5,
@@ -91,15 +97,16 @@ class _DecklistPageState extends State<DecklistPage> {
                     physics: const NeverScrollableScrollPhysics(),
                     // ignore: sort_child_properties_last
                     children: List.generate(widget.decklist[1].length, (index) {
-                      return CardView(widget.decklist[1][index]);
+                      return CardView(widget.decklist[1][index], index,
+                          removeItems, removedExtra);
                     }),
-                    crossAxisCount: widget.decklist[1].length,
+                    crossAxisCount: 15,
                   ),
                 )
               ],
             ),
             const SizedBox(
-              width: 10,
+              width: 32,
             ),
             Column(
               children: [
@@ -107,7 +114,7 @@ class _DecklistPageState extends State<DecklistPage> {
                 Container(
                   color: Colors.lightGreenAccent.withOpacity(0.4),
                   width: 200,
-                  height: 600,
+                  height: 530,
                   child: Column(children: [
                     const Text(
                       'Main Deck Cost',
@@ -155,6 +162,72 @@ class _DecklistPageState extends State<DecklistPage> {
                       style: const TextStyle(fontSize: 18),
                     ),
                   ]),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 180,
+                  width: 200,
+                  color: Colors.redAccent.withOpacity(0.4),
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      const Text(
+                        'Remove Cards',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      ),
+                      Switch(
+                          value: removeItems,
+                          onChanged: (bool newBool) {
+                            setState(() {
+                              removeItems = newBool;
+                            });
+                          }),
+                      Container(
+                        width: 200,
+                        height: 100,
+                        padding: const EdgeInsets.all(5),
+                        child: Visibility(
+                            visible: removeItems,
+                            child: Row(
+                              children: [
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.grey),
+                                  onPressed: () {},
+                                  child: const Text('Remove'),
+                                ),
+                                const SizedBox(
+                                  width: 15,
+                                ),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.grey),
+                                  onPressed: () {
+                                    setState(() {
+                                      removeItems = false;
+                                      for (var i in removedMain) {
+                                        widget.decklist[0].removeAt(i);
+                                      }
+                                      for (var i in removedExtra) {
+                                        widget.decklist[1].removeAt(i);
+                                      }
+                                      for (var i in removedSide) {
+                                        widget.decklist[2].removeAt(i);
+                                      }
+                                    });
+                                  },
+                                  child: const Text('Confirm'),
+                                )
+                              ],
+                            )),
+                      )
+                    ],
+                  ),
                 )
               ],
             )
@@ -165,10 +238,18 @@ class _DecklistPageState extends State<DecklistPage> {
   }
 }
 
-class CardView extends StatelessWidget {
-  const CardView(this.card, {super.key});
+class CardView extends StatefulWidget {
+  CardView(this.card, this.index, this.remove, this.tobeRemoved, {super.key});
   final YGOCard card;
+  List tobeRemoved;
+  bool remove;
+  int index;
+  @override
+  State<CardView> createState() => _CardViewState();
+}
 
+class _CardViewState extends State<CardView> {
+  bool selected = false;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -179,19 +260,48 @@ class CardView extends StatelessWidget {
             width: 100,
             height: 35,
             child: Text(
-              card.name,
+              widget.card.name,
               style: const TextStyle(fontSize: 9, fontWeight: FontWeight.bold),
             )),
         const SizedBox(
           height: 5,
         ),
         Image.network(
-          card.img,
+          widget.card.img,
           scale: 0.6,
         ),
-        Text(
-          card.price,
-          style: const TextStyle(fontSize: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              widget.card.price,
+              style: const TextStyle(fontSize: 10),
+            ),
+            const SizedBox(
+              width: 2,
+            ),
+            Visibility(
+              visible: widget.remove,
+              child: SizedBox(
+                  width: 10,
+                  height: 10,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              selected ? Colors.red : Colors.green),
+                      onPressed: () {
+                        if (widget.tobeRemoved.contains(widget.index)) {
+                          widget.tobeRemoved.remove(widget.index);
+                          selected = false;
+                        } else {
+                          widget.tobeRemoved.add(widget.index);
+                          selected = true;
+                        }
+                        setState(() {});
+                      },
+                      child: const Text('*'))),
+            )
+          ],
         )
       ]),
     );
